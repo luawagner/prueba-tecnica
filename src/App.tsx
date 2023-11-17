e79a4c5c646aff69a923c78953c532d905aa65f6
ludmila wagner <lu.wagner91@gmail.com>
2023-11-17 11:13:46 -0300

    ordenar usuarios por países

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { UsersList } from './components/UsersList'
+import { User } from './types'
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [showColors, setShowColors] = useState(false)
+  const [users, setUsers] = useState<User[]>([]);
+  const [showColors, setShowColors] = useState(false);
+  const [sortByCountry, setSortByCountry] = useState(false)
+
+  //actualización del estado basado en el estado previo
   const toggleColors = () => {
     setShowColors(!showColors) //cuando se ejecute invierte el estado actual
   }
+
+  //El estado antes de la actualización. Compara prevState con this.state para determinar lo que cambió.
+  const toggleSortByCountry = () => {
+    setSortByCountry(prevState => !prevState)
+  }
+ 
 
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
@@ -22,15 +32,26 @@ function App() {
   }, [])
 //Para tipar los datos de la Api, copio el raw y lo pego en quicktype
 
+const sortedUsers = sortByCountry 
+? users.toSorted((a, b) => {
+  return a.location.country.localeCompare(b.location.country)
+})
+: users
+
   return (
     
     <div className='App'>
+      <h1>Prueba técnica</h1>
       <header>
     <button onClick={toggleColors}>Colorear filas</button>
+    <button onClick={toggleSortByCountry}>
+      {sortByCountry ? 'No ordenar' : 'Ordenar por país'}
+      </button>
+
       </header>
       <main>
-    <h1>Prueba técnica</h1>
-    <UsersList showColors={showColors} users={users} />
+    
+    <UsersList showColors={showColors} users={sortedUsers} />
     </main>
    </div>
    
