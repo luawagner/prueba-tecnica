1e166d308df6aa264033d8a8b6296a2cc5fc7bb3
ludmila wagner <lu.wagner91@gmail.com>
2023-11-20 12:38:42 -0300

    ordenar usuarios por país

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useMemo } from 'react'
 import './App.css'
 import { UsersList } from './components/UsersList'
 import { User } from './types'
 
 function App() {
+
+  //-------------- ESTADOS
   const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
   const [sortByCountry, setSortByCountry] = useState(false)
+  const [filterCountry, setFilterCountry] = useState<string | null>(null)
+
   const originalUsers = useRef<User[]>([])
   //userRef -> guarda un valor que queremos que se comparta
   //entre renderizados pero que, al cambiar, no vuelva a renderizar el componente
   //Al llamar a la API guardamos esta referencia para disponer de ella
   //cuando el estado users cambie.
+
+
+  //----------------  HANDLES
 
   //actualización del estado basado en el estado previo
   const toggleColors = () => {
@@ -36,7 +43,9 @@ function App() {
   const filteredUsers = users.filter((user) => user.email !== email)
   setUsers(filteredUsers)
  }
- 
+
+ //------------------FETCHING DE DATOS
+
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
     .then(async res => await res.json())
@@ -50,12 +59,42 @@ function App() {
   }, [])
 //Para tipar los datos de la Api, copio el raw y lo pego en quicktype
 
+//-------------------PRIMERO FILTRAMOS LOS USUARIOS
+
+/*Le indico que quiero que se ejecute solo cuando cambien los usuarios
+o cada vez que el usuario me pase un nuevo filterCountry 
+comparamos con toLowerCase para que no haya diferencias de caracteres. 
+Usamos useMemo() para evitar cálculos innecesarios. */
+const filteredUsers = useMemo(() => {
+  return filterCountry !== null && filterCountry.length > 0
+  //si tenemos un string y es mayor que 0s
+  ? users.filter((user => {
+    return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
+  }))
+  : users
+}, [users, filterCountry])
+ 
+
+
 //tener un solo estado para aquí hacer los cálculos necesarios con ese estado
-const sortedUsers = sortByCountry 
-? users.toSorted((a, b) => {
-  return a.location.country.localeCompare(b.location.country)
-})
-: users
+//---------------------SEGUNDO ORDENAMOS LOS USUARIOS
+/*quiero que memorice el valor de la constante sortedUsers
+y que no lo vuelva a calcular entre renderizados a no ser que el valor 
+de filteredUsers o de sortByCountry cambie.
+Le paso filteredUsers que va a darle los usuarios ya filtrados
+o, si no hay un filtro aplicado, la lista completa de usuarios*/
+
+const sortedUsers = useMemo(() => {
+  return sortByCountry 
+  ? filteredUsers.toSorted(
+    (a, b) => 
+     a.location.country.localeCompare(b.location.country)
+  )
+  : filteredUsers
+}, [filteredUsers, sortByCountry]) /*si coloreo filas (showColors) se vuelve a renderizar el componente
+pero esta función no se vuelve a ejecutar porque no cambió ninguno de los estados 
+que le indiqué que vigile. */
+
 
   return (
     
@@ -68,6 +107,10 @@ const sortedUsers = sortByCountry
       {sortByCountry ? 'No ordenar' : 'Ordenar por país'}
       </button>
 
+      {/*cada vez que se escriba en este input se cambiará el estado del filterCountry*/}
+    <input placeholder='Filtra por país' onChange={(e) => {
+      setFilterCountry(e.target.value) // acá tenemos el string que está escribiendo el usuario
+    }} />
       </header>
       <main>
     
